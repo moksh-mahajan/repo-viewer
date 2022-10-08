d951c24c4c0222b1006d6504b1acae499139daca
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-10-08 20:35:50 +0530

    feat: add sign out functionality

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,10 +1,14 @@
+import 'dart:convert';
+
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 import 'package:oauth2/oauth2.dart';
 import 'package:repo_viewer/auth/domain/auth_failure.dart';
 import 'package:repo_viewer/auth/infrastructure/credentials_storage/credentials_storage.dart';
 import 'package:repo_viewer/auth/infrastructure/oauth_app_credentials.dart';
 import 'package:http/http.dart' as http;
+import 'package:repo_viewer/core/shared/encoders.dart';
 
 class GithubOAuthHttpClient extends http.BaseClient {
   final httpClient = http.Client();
@@ -18,8 +22,12 @@ class GithubOAuthHttpClient extends http.BaseClient {
 
 class GithubAuthenticator {
   final CredentialsStorage _credentialsStorage;
+  final Dio _dio;
 
-  GithubAuthenticator(this._credentialsStorage);
+  GithubAuthenticator(
+    this._credentialsStorage,
+    this._dio,
+  );
 
   static const scopes = ['read:user', 'repo'];
   static final authorizationEndpoint =
@@ -27,6 +35,8 @@ class GithubAuthenticator {
   static final tokenEndpoint =
       Uri.parse('https://github.com/login/oauth/access_token');
   static final redirectUrl = Uri.parse('http://localhost:3000/callback');
+  static final revocationEndpoint = Uri.parse(
+      'https://api.github.com/applications/${OauthAppCredentials.clientId}/token');
 
   Future<Credentials?> getSignedInCredentials() async {
     try {
@@ -70,6 +80,34 @@ class GithubAuthenticator {
       return left(const AuthFailure.server());
     } on AuthorizationException catch (e) {
       return left(AuthFailure.server('${e.error}: ${e.description}'));
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
+
+  Future<Either<AuthFailure, Unit>> signOut() async {
+    final accessToken = await _credentialsStorage
+        .read()
+        .then((credentials) => credentials?.accessToken);
+
+    final usernameAndPassword = stringToBase64.encode(
+      '${OauthAppCredentials.clientId}:${OauthAppCredentials.clientSecret}',
+    );
+
+    try {
+      _dio.deleteUri(
+        revocationEndpoint,
+        options: Options(
+          headers: {
+            'Authorization': 'basic $usernameAndPassword',
+          },
+        ),
+        data: {
+          'access_token': accessToken,
+        },
+      );
+      await _credentialsStorage.clear();
+      return right(unit);
     } on PlatformException {
       return left(const AuthFailure.storage());
     }
