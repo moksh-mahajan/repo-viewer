40e199b9cbcffa2882eefb60a00c209c3710861f
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-11-27 16:40:23 +0530

    ui: splash page design

--- lib/splash/presentation/splash_page.dart
+++ lib/splash/presentation/splash_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 48.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(
+              MdiIcons.github,
+              size: 150,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            LinearProgressIndicator(),
+          ],
+        ),
+      ),
+    );
   }
 }
