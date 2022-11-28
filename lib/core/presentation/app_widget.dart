512fdbcbb38193991f56bf15b2a967d08a17d963
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-11-28 13:16:06 +0530

    navigation based on auth state

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,12 +1,37 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/application/auth_notifier.dart';
+import 'package:repo_viewer/auth/shared/providers.dart';
 import 'package:repo_viewer/core/presentation/routes/app_router.gr.dart';
 
-class AppWidget extends StatelessWidget {
+final initializationProvider = FutureProvider<Unit>((ref) async {
+  final authNotifier = ref.read(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateAuthStatus();
+  return unit;
+});
+
+class AppWidget extends ConsumerWidget {
   AppWidget({Key? key}) : super(key: key);
   final _appRouter = AppRouter();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(initializationProvider, (_, __) {});
+    ref.listen<AuthState>(authNotifierProvider, (_, state) {
+      state.maybeMap(
+        orElse: () {},
+        authenticated: (_) => _appRouter.pushAndPopUntil(
+          const StarredReposRoute(),
+          predicate: (_) => false,
+        ),
+        unauthenticated: (_) => _appRouter.pushAndPopUntil(
+          const SignInRoute(),
+          predicate: (_) => false,
+        ),
+      );
+    });
+
     return MaterialApp.router(
       title: 'Repo Viewer',
       routerDelegate: _appRouter.delegate(),
