8227ca5b234f6506d1aaa3376f1168432456f687
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-12-04 13:45:18 +0530

    github feature folder structure

--- lib/core/presentation/routes/app_router.dart
+++ lib/core/presentation/routes/app_router.dart
@@ -2,7 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:repo_viewer/auth/presentation/authorization_page.dart';
 import 'package:repo_viewer/auth/presentation/sign_in_page.dart';
 import 'package:repo_viewer/splash/presentation/splash_page.dart';
-import 'package:repo_viewer/starred_repos/presentation/starred_repos_page.dart';
+import 'package:repo_viewer/github/repos/starred_repos/presentation/starred_repos_page.dart';
 
 @MaterialAutoRouter(routes: [
   MaterialRoute(page: SplashPage, initial: true),
