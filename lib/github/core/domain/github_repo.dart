e41cd2eb3f561dc47c81774b542454411c5dda8e
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-12-04 13:59:52 +0530

    github feature entities and failures

---
+++ lib/github/core/domain/github_repo.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:repo_viewer/github/core/domain/user.dart';
+part 'github_repo.freezed.dart';
+
+@freezed
+class GithubRepo with _$GithubRepo {
+  const GithubRepo._();
+  const factory GithubRepo({
+    required User owner,
+    required String name,
+    required String description,
+    required int stargazersCount,
+  }) = _GithubRepo;
+
+  String get fullName => '${owner.name}/$name';
+}
