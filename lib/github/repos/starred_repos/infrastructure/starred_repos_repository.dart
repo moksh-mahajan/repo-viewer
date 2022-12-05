542e6c54c7403fbfffcb04a95766022bc7e560d4
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-12-05 20:51:49 +0530

    starred_repos_repository implementation

---
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_repository.dart
@@ -0,0 +1,51 @@
+import 'package:dartz/dartz.dart';
+import 'package:repo_viewer/core/domain/fresh.dart';
+import 'package:repo_viewer/core/infrastructure/network_exceptions.dart';
+import 'package:repo_viewer/github/core/domain/github_failure.dart';
+import 'package:repo_viewer/github/core/domain/github_repo.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
+import 'package:repo_viewer/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart';
+
+class StarredReposRepository {
+  final StarredReposRemoteService _remoteService;
+  // TODO: local service
+
+  StarredReposRepository(this._remoteService);
+
+  Future<Either<GithubFailure, Fresh<List<GithubRepo>>>> getStarredReposPage(
+    int page,
+  ) async {
+    try {
+      final remotePageItems = await _remoteService.getStarredReposPage(page);
+      return right(
+        remotePageItems.when(
+          // TODO: local service
+          noConnection: (maxPage) => Fresh.no(
+            [],
+            isNextPageAvailable: page < maxPage!,
+          ),
+          // TODO: local service
+          notModified: (maxPage) => Fresh.yes(
+            [],
+            isNextPageAvailable: page < maxPage!,
+          ),
+          withNewData: (data, maxPage) {
+            // TODO: save data in the local service
+            return Fresh.yes(
+              data.toDomain(),
+              isNextPageAvailable: page < maxPage!,
+            );
+          },
+        ),
+      );
+    } on RestApiException catch (e) {
+      return left(GithubFailure.api(e.errorCode));
+    }
+  }
+}
+
+extension DTOListTODomainList on List<GithubRepoDTO> {
+  List<GithubRepo> toDomain() {
+    return map((e) => e.toDomain()).toList();
+  }
+}
