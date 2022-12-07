1cb97bc6e1aaa4102910476594ee195b31a2e5c5
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-12-07 22:27:59 +0530

    starred_repos feature providers

---
+++ lib/github/core/shared/providers.dart
@@ -0,0 +1,35 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/core/shared/providers.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers_cache.dart';
+import 'package:repo_viewer/github/repos/starred_repos/application/starred_repos_notifier.dart';
+import 'package:repo_viewer/github/repos/starred_repos/infrastructure/starred_repos_local_service.dart';
+import 'package:repo_viewer/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart';
+import 'package:repo_viewer/github/repos/starred_repos/infrastructure/starred_repos_repository.dart';
+
+final githubHeadersCacheProvider = Provider(
+  (ref) => GithubHeadersCache(ref.watch(sembastProvider)),
+);
+
+final starredReposLocalServiceProvider = Provider(
+  (ref) => StarredReposLocalService(ref.watch(sembastProvider)),
+);
+
+final starredReposRemoteServiceProvider = Provider(
+  (ref) => StarredReposRemoteService(
+    ref.watch(dioProvider),
+    ref.watch(githubHeadersCacheProvider),
+  ),
+);
+
+final starredReposRepositoryProvider = Provider(
+  (ref) => StarredReposRepository(
+    ref.watch(starredReposRemoteServiceProvider),
+    ref.watch(starredReposLocalServiceProvider),
+  ),
+);
+
+final starredReposNotifierProvider = StateNotifierProvider(
+  (ref) => StarredReposNotifier(
+    ref.watch(starredReposRepositoryProvider),
+  ),
+);
