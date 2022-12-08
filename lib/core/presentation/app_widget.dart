a3885e7dc90668ad00fd4939f9855aeca444a551
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-12-08 21:49:01 +0530

    configuring dio for validating status codes

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -14,6 +14,8 @@ final initializationProvider = FutureProvider<Unit>((ref) async {
       headers: {
         'Accept': 'application/vnd.github.v3.html+json',
       },
+      validateStatus: (status) =>
+          status != null && status >= 200 && status < 400,
     )
     ..interceptors.add(
       ref.read(oAuth2InterceptorProvider),
