e8ad6b83e6936389c51b64d1a285de819a90c3be
Moksh Mahajan <mokshmahajan008@gmail.com>
2022-12-11 22:13:39 +0530

    removing code duplication in starred and searched repos feature

---
+++ lib/github/repos/core/infrastructure/repos_remote_service.dart
@@ -0,0 +1,60 @@
+import 'package:dio/dio.dart';
+import 'package:repo_viewer/core/infrastructure/dio_extensions.dart';
+import 'package:repo_viewer/core/infrastructure/network_exceptions.dart';
+import 'package:repo_viewer/core/infrastructure/remote_response.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers_cache.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
+import 'package:repo_viewer/github/core/infrastructure/pagination_config.dart';
+
+abstract class ReposRemoteService {
+  final Dio _dio;
+  final GithubHeadersCache _headersCache;
+
+  ReposRemoteService(
+    this._dio,
+    this._headersCache,
+  );
+
+  Future<RemoteResponse<List<GithubRepoDTO>>> getPage({
+    required Uri requestUri,
+    required List<dynamic> Function(dynamic json) jsonDataSelector,
+  }) async {
+    final previousHeaders = await _headersCache.getHeaders(requestUri);
+
+    try {
+      final response = await _dio.getUri(
+        requestUri,
+        options: Options(
+          headers: {
+            'If-None-Match': previousHeaders?.etag ?? '',
+          },
+        ),
+      );
+      if (response.statusCode == 304) {
+        return RemoteResponse.notModified(
+            maxPage: previousHeaders?.link?.maxPage ?? 0);
+      } else if (response.statusCode == 200) {
+        final headers = GithubHeaders.parse(response);
+        await _headersCache.saveHeaders(requestUri, headers);
+        final convertedData = jsonDataSelector (response.data)
+            .map((e) => GithubRepoDTO.fromJson(e as Map<String, dynamic>))
+            .toList();
+        return RemoteResponse.withNewData(
+          convertedData,
+          maxPage: headers.link?.maxPage ?? 1,
+        );
+      } else {
+        throw RestApiException(response.statusCode);
+      }
+    } on DioError catch (e) {
+      if (e.isNoConnectionError) {
+        return const RemoteResponse.noConnection();
+      } else if (e.response != null) {
+        throw RestApiException(e.response!.statusCode);
+      } else {
+        rethrow;
+      }
+    }
+  }
+}
--- lib/github/repos/searched_repos/infrastructure/searched_repos_remote_service.dart
+++ lib/github/repos/searched_repos/infrastructure/searched_repos_remote_service.dart
@@ -5,22 +5,25 @@ import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
 import 'package:repo_viewer/github/core/infrastructure/pagination_config.dart';
 import 'package:repo_viewer/github/repos/core/infrastructure/repos_remote_service.dart';
 
-class StarredReposRemoteService extends ReposRemoteService {
-  StarredReposRemoteService(
+class SearchedReposRemoteService extends ReposRemoteService {
+  SearchedReposRemoteService(
     Dio dio,
     GithubHeadersCache headersCache,
   ) : super(dio, headersCache);
 
-  Future<RemoteResponse<List<GithubRepoDTO>>> getStarredReposPage(
-          int page) async =>
+  Future<RemoteResponse<List<GithubRepoDTO>>> getSearchedReposPage(
+    String query,
+    int page,
+  ) async =>
       super.getPage(
           requestUri: Uri.https(
             'api.github.com',
-            '/user/starred',
+            '/search/repositories',
             {
+              'q': query,
               'page': '$page',
               'per_page': PaginationConfig.itemsPerPage.toString(),
             },
           ),
-          jsonDataSelector: (json) => json as List<dynamic>);
+          jsonDataSelector: (json) => json['items'] as List<dynamic>);
 }
